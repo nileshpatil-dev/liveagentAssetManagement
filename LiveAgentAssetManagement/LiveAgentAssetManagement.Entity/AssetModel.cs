e295f46dbcea2aee1b4e2763268a36f5589a91d4
Nilesh Patil <nileshpatil.work@gmail.com>
2017-06-24 14:17:39 +0530

    Added class library for entities

---
+++ LiveAgentAssetManagement/LiveAgentAssetManagement.Entity/AssetModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveAgentAssetManagement.Entity
+{
+    public class AssetModel
+    {
+        [Display(Name = "Id")]
+        public int AssetId { get; set; }
+
+        [Required(ErrorMessage = "Asset should not be empty")]
+        public string AssetName { get; set; }
+
+        [Required(ErrorMessage = "Asset serial number not be empty")]
+        public string AssetSrNo { get; set; }
+
+        [Range(1, Int32.MaxValue, ErrorMessage = "Invalid asset department id")]
+        public int AssetDepartment { get; set; }
+
+        [Range(1, Int32.MaxValue, ErrorMessage = "Invalid asset category id")]
+        public int AssetCategory { get; set; }
+
+        [Range(1, Int32.MaxValue, ErrorMessage = "Invalid asset brand id")]
+        public int AssetBrand { get; set; }
+
+        public bool AssetStatus { get; set; }
+
+        [Range(1, Int32.MaxValue, ErrorMessage = "Invalid asset supplier id")]
+        public int AssetSupplier { get; set; }
+
+        [Required(ErrorMessage = "Invalid asset purchased date")]
+        public DateTime PurchasedDate { get; set; }
+    }
+}
---
+++ LiveAgentAssetManagement/LiveAgentAssetManagement/Controllers/AssetController.cs
@@ -0,0 +1,99 @@
+﻿using LiveAgentAssetManagement.BLL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiveAgentAssetManagement.Controllers
+{
+    public class AssetController : Controller
+    {
+        private readonly IAssetManagementService assetManagementService;
+        public AssetController(IAssetManagementService assetManagementService)
+        {
+
+            this.assetManagementService = assetManagementService;
+        }
+
+        // GET: Asset
+        public ActionResult Index()
+        {
+            DataTable dt = assetManagementService.GetAssets();
+            return View();
+        }
+
+        // GET: Asset/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Asset/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Asset/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asset/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Asset/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asset/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Asset/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
